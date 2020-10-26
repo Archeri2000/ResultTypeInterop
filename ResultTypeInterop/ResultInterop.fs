bcca6c980ea4d9c8ed5c3e3d8182b7df0a8d3ba9
Archery2000 <ongyuhann@hotmail.com>
2020-10-26 22:29:02 +0800

    Fix Namespacing

--- ResultTypeInterop/ResultInterop.fs
+++ ResultTypeInterop/ResultInterop.fs
@@ -4,20 +4,63 @@ open System.Threading.Tasks
 open CSharp_Result
 open System.Runtime.CompilerServices
 
-module ResultInterop =
-    // Result
+// Result
+[<Extension>]
+type ResultExtensions =
+    /// <summary>
+    /// Converts a C# Result to an F# Result
+    /// </summary>
+    /// <param name="result">C# result to convert</param>
+    /// <typeparam name="'T">Success type of result</typeparam>
+    /// <returns>F# Result</returns>
     [<Extension>]
-    type ResultExtensions =
-        /// <summary>
-        /// Converts a C# Result to an F# Result
-        /// </summary>
-        /// <param name="result">C# result to convert</param>
-        /// <typeparam name="'T">Success type of result</typeparam>
-        /// <returns>F# Result</returns>
-        [<Extension>]
-        static member inline ToFSharpResult (result: Result<'T>) =
-            result.Match(Ok, Error)
-            
+    static member inline ToFSharpResult (result: Result<'T>) =
+        result.Match(Ok, Error)
+    
+// Result Collection
+[<Extension>]
+type ResultListExtensions =
+    /// <summary>
+    /// Converts a C# Result IEnumerable to a F# Result List
+    /// </summary>
+    /// <param name="list">C# Result IEnumerable to convert</param>
+    /// <typeparam name="'T">Success type of result</typeparam>
+    /// <returns>F# result list</returns>
+    [<Extension>]
+    static member inline ToFSharpResultList (list: seq<Result<'T>>) =
+        list
+        |> Seq.map (fun x -> x.ToFSharpResult())
+        |> Seq.toList
+    
+// Async Result Type       
+[<Extension>]  
+type AsyncResultExtensions =
+    /// <summary>
+    /// Converts a C# Result Task (Async Result) to an Async F# Result
+    /// </summary>
+    /// <param name="task">C# Result Task (Async Result) to convert</param>
+    /// <typeparam name="'T">Success type of result</typeparam>
+    /// <returns>Async F# result</returns>
+    [<Extension>]
+    static member ToFSharpAsyncResult (task: Task<Result<'T>>) =
+        AsyncInterop.asAsyncT (task.ContinueWith(fun (x:Task<Result<'T>>) -> x.Result.ToFSharpResult()), None);
+    
+// Async Result Collection
+[<Extension>]  
+type AsyncResultListExtensions =
+    /// <summary>
+    /// Converts a C# Result Task (Async Result) IEnumerable to an Async F# Result List
+    /// </summary>
+    /// <param name="list">C# Result Task (Async Result) IEnumerable to convert</param>
+    /// <typeparam name="'T">Success type of result</typeparam>
+    /// <returns>Async F# result list</returns>
+    [<Extension>]
+    static member ToFSharpAsyncResultList (list: seq<Task<Result<'T>>>) =
+        list
+        |> Seq.map (fun x -> x.ToFSharpAsyncResult())
+        |> Seq.toList
+
+module FSharpConvert =
     /// <summary>
     /// Converts a F# Result to a C# Result
     /// </summary>
@@ -26,24 +69,9 @@ module ResultInterop =
     /// <returns>C# Result</returns>
     let ToCSharpResult (result:Result<'T, exn>): Result<'T> =
         match result with
-        |Ok success -> success :> Result<'T>
-        |Error failure -> new Failure<'T>(failure) :> Result<'T>
+        |Ok success -> (Success<'T>(success)) :> Result<'T> 
+        |Error failure -> (Failure<'T>(failure)) :> Result<'T>
         
-    // Result Collection
-    [<Extension>]
-    type ResultListExtensions =
-        /// <summary>
-        /// Converts a C# Result IEnumerable to a F# Result List
-        /// </summary>
-        /// <param name="list">C# Result IEnumerable to convert</param>
-        /// <typeparam name="'T">Success type of result</typeparam>
-        /// <returns>F# result list</returns>
-        [<Extension>]
-        static member inline ToFSharpResultList (list: seq<Result<'T>>) =
-            list
-            |> Seq.map (fun x -> x.ToFSharpResult())
-            |> Seq.toList
-            
     /// <summary>
     /// Converts a F# Result List to a C# Result IEnumerable
     /// </summary>
@@ -55,19 +83,6 @@ module ResultInterop =
         |> List.map ToCSharpResult
         |> Seq.ofList
         
-    // Async Result Type       
-    [<Extension>]  
-    type AsyncResultExtensions =
-        /// <summary>
-        /// Converts a C# Result Task (Async Result) to an Async F# Result
-        /// </summary>
-        /// <param name="task">C# Result Task (Async Result) to convert</param>
-        /// <typeparam name="'T">Success type of result</typeparam>
-        /// <returns>Async F# result</returns>
-        [<Extension>]
-        static member ToFSharpAsyncResult (task: Task<Result<'T>>) =
-            AsyncInterop.asAsyncT (task.ContinueWith(fun (x:Task<Result<'T>>) -> x.Result.ToFSharpResult()), None);
-            
     /// <summary>
     /// Converts an Async F# Result to a C# Result Task (Async Result)
     /// </summary>
@@ -81,21 +96,6 @@ module ResultInterop =
         }
         mapped.AsTask()
         
-    // Async Result Collection
-    [<Extension>]  
-    type AsyncResultListExtensions =
-        /// <summary>
-        /// Converts a C# Result Task (Async Result) IEnumerable to an Async F# Result List
-        /// </summary>
-        /// <param name="list">C# Result Task (Async Result) IEnumerable to convert</param>
-        /// <typeparam name="'T">Success type of result</typeparam>
-        /// <returns>Async F# result list</returns>
-        [<Extension>]
-        static member ToFSharpAsyncResultList (list: seq<Task<Result<'T>>>) =
-            list
-            |> Seq.map (fun x -> x.ToFSharpAsyncResult())
-            |> Seq.toList
-
     /// <summary>
     /// Converts an Async F# Result List to a C# Result Task (Async Result) IEnumerable
     /// </summary>
