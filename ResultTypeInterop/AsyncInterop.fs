5e2de9fd147a68a5c927e6e28619af86dca458d9
Archery2000 <ongyuhann@hotmail.com>
2020-10-26 19:22:32 +0800

    Add Documentation

--- ResultTypeInterop/AsyncInterop.fs
+++ ResultTypeInterop/AsyncInterop.fs
@@ -6,6 +6,12 @@ open System.Threading
 open System.Threading.Tasks
 
 module private AsyncInterop =
+    /// <summary>
+    /// Converts a F# Async into a .NET Task
+    /// </summary>
+    /// <param name="async">Async function to convert</param>
+    /// <typeparam name="'T">Return type of function</typeparam>
+    /// <returns>.NET Task that holds the function</returns>
     let asTask(async: Async<'T>, token: CancellationToken option) =
         let tcs = TaskCompletionSource<'T>()
         let token = defaultArg token (CancellationToken());
@@ -14,6 +20,12 @@ module private AsyncInterop =
                 tcs.SetException, token)
         tcs.Task
  
+    /// <summary>
+    /// Converts a .NET Task to an F# Async
+    /// </summary>
+    /// <param name="task">.NET task to convert</param>
+    /// <param name="token">Cancellation Token for function</param>
+    /// <returns>F# Async function</returns>
     let asAsync(task: Task, token: CancellationToken option) =
        Async.FromContinuations( 
                 fun (completed, caught, canceled) ->
@@ -25,6 +37,13 @@ module private AsyncInterop =
                       else completed()), token)
                         |> ignore)
 
+    /// <summary>
+    /// Converts a .NET Task to an F# Async
+    /// </summary>
+    /// <param name="task">.NET task to convert</param>
+    /// <param name="token">Cancellation Token for function</param>
+    /// <typeparam name="'T">Return type of function</typeparam>
+    /// <returns>F# Async function</returns>
     let asAsyncT(task: Task<'T>, token: CancellationToken option) =
         Async.FromContinuations( 
                  fun (completed, caught, canceled) ->
@@ -38,16 +57,42 @@ module private AsyncInterop =
 
 [<Extension>]  
 type AsyncInteropExtensions =
+    /// <summary>
+    /// Converts a .NET Task to an F# Async
+    /// </summary>
+    /// <param name="task">.NET task to convert</param>
+    /// <typeparam name="'T">Return type of function</typeparam>
+    /// <returns>F# Async function</returns>
     [<Extension>]
     static member AsAsync (task: Task<'T>) = AsyncInterop.asAsyncT (task, None) 
 
+    /// <summary>
+    /// Converts a .NET Task to an F# Async
+    /// </summary>
+    /// <param name="task">.NET task to convert</param>
+    /// <param name="token">Cancellation Token for function</param>
+    /// <typeparam name="'T">Return type of function</typeparam>
+    /// <returns>F# Async function</returns>
     [<Extension>]
     static member AsAsync (task: Task<'T>, token: CancellationToken) =
         AsyncInterop.asAsyncT (task, Some token) 
 
+    /// <summary>
+    /// Converts a F# Async into a .NET Task
+    /// </summary>
+    /// <param name="async">Async function to convert</param>
+    /// <typeparam name="'T">Return type of function</typeparam>
+    /// <returns>.NET Task that holds the function</returns>
     [<Extension>]
     static member AsTask (async: Async<'T>) = AsyncInterop.asTask (async, None)
 
+    /// <summary>
+    /// Converts a F# Async into a .NET Task
+    /// </summary>
+    /// <param name="async">Async function to convert</param>
+    /// <param name="token">Cancellation Token for function</param>
+    /// <typeparam name="'T">Return type of function</typeparam>
+    /// <returns>.NET Task that holds the function</returns>
     [<Extension>]
     static member AsTask (async: Async<'T>, token: CancellationToken) =
         AsyncInterop.asTask (async, Some token) 
--- ResultTypeInterop/ResultInterop.fs
+++ ResultTypeInterop/ResultInterop.fs
@@ -8,11 +8,22 @@ module ResultInterop =
     // Result
     [<Extension>]
     type ResultExtensions =
-        
+        /// <summary>
+        /// Converts a C# Result to an F# Result
+        /// </summary>
+        /// <param name="result">C# result to convert</param>
+        /// <typeparam name="'T">Success type of result</typeparam>
+        /// <returns>F# Result</returns>
         [<Extension>]
         static member inline ToFSharpResult (result: Result<'T>) =
             result.Match(Ok, Error)
             
+    /// <summary>
+    /// Converts a F# Result to a C# Result
+    /// </summary>
+    /// <param name="result">F# result to convert</param>
+    /// <typeparam name="'T">Success type of result</typeparam>
+    /// <returns>C# Result</returns>
     let ToCSharpResult (result:Result<'T, exn>): Result<'T> =
         match result with
         |Ok success -> success :> Result<'T>
@@ -21,12 +32,24 @@ module ResultInterop =
     // Result Collection
     [<Extension>]
     type ResultListExtensions =
+        /// <summary>
+        /// Converts a C# Result IEnumerable to a F# Result List
+        /// </summary>
+        /// <param name="list">C# Result IEnumerable to convert</param>
+        /// <typeparam name="'T">Success type of result</typeparam>
+        /// <returns>F# result list</returns>
         [<Extension>]
         static member inline ToFSharpResultList (list: seq<Result<'T>>) =
             list
             |> Seq.map (fun x -> x.ToFSharpResult())
             |> Seq.toList
             
+    /// <summary>
+    /// Converts a F# Result List to a C# Result IEnumerable
+    /// </summary>
+    /// <param name="list">F# result list to convert</param>
+    /// <typeparam name="'T">Success type of result</typeparam>
+    /// <returns>C# Result IEnumerable</returns>
     let ToCSharpResultList (list:Result<'T, exn> list) =
         list
         |> List.map ToCSharpResult
@@ -35,11 +58,22 @@ module ResultInterop =
     // Async Result Type       
     [<Extension>]  
     type AsyncResultExtensions =
+        /// <summary>
+        /// Converts a C# Result Task (Async Result) to an Async F# Result
+        /// </summary>
+        /// <param name="task">C# Result Task (Async Result) to convert</param>
+        /// <typeparam name="'T">Success type of result</typeparam>
+        /// <returns>Async F# result</returns>
         [<Extension>]
         static member ToFSharpAsyncResult (task: Task<Result<'T>>) =
             AsyncInterop.asAsyncT (task.ContinueWith(fun (x:Task<Result<'T>>) -> x.Result.ToFSharpResult()), None);
             
-
+    /// <summary>
+    /// Converts an Async F# Result to a C# Result Task (Async Result)
+    /// </summary>
+    /// <param name="task">Async F# result to convert</param>
+    /// <typeparam name="'T">Success type of result</typeparam>
+    /// <returns>C# Result Task (Async Result)</returns>
     let ToCSharpAsyncResult (task: Async<Result<'T,exn>>) : Task<Result<'T>> =
         let mapped = async{
             let! result = task
@@ -50,12 +84,24 @@ module ResultInterop =
     // Async Result Collection
     [<Extension>]  
     type AsyncResultListExtensions =
+        /// <summary>
+        /// Converts a C# Result Task (Async Result) IEnumerable to an Async F# Result List
+        /// </summary>
+        /// <param name="list">C# Result Task (Async Result) IEnumerable to convert</param>
+        /// <typeparam name="'T">Success type of result</typeparam>
+        /// <returns>Async F# result list</returns>
         [<Extension>]
         static member ToFSharpAsyncResultList (list: seq<Task<Result<'T>>>) =
             list
             |> Seq.map (fun x -> x.ToFSharpAsyncResult())
             |> Seq.toList
 
+    /// <summary>
+    /// Converts an Async F# Result List to a C# Result Task (Async Result) IEnumerable
+    /// </summary>
+    /// <param name="list">Async F# result list to convert</param>
+    /// <typeparam name="'T">Success type of result</typeparam>
+    /// <returns>C# Result Task (Async Result) IEnumerable</returns>
     let ToCSharpAsyncResultList (list: Async<Result<'T,exn>> list) =
         list
         |> List.map ToCSharpAsyncResult
